84be3c90c77e9914d77d0f2f5b32fc736a9be253
Kevin Smith <kevinl2code@gmail.com>
2022-01-12 16:12:29 -0600

    feat: added a table with all daily entries to dashboard as a temporary filler

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,14 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@mui/material'
 import React, { useCallback, useEffect } from 'react'
-import { DailyEntry } from '../../model/Model'
+import { ActivityLevel, DailyEntry } from '../../model/Model'
 import { DataService } from '../../services/DataService'
 import { Calculate } from '../../utilities/Calculate'
 
@@ -20,9 +29,50 @@ export const DashboardPage: React.FC<Props> = (props: Props) => {
 
   console.log(entries)
   console.log(calculate.TDEE(1000, 'SEDENTARY'))
+
+  if (!entries) {
+    return <h1>No entries</h1>
+  }
+
+  const generatedRows = entries.map((entry, index) => {
+    const confirmedMeals = entry.meals.length > 0 ? entry.meals : null
+    const caloriesConsumed =
+      confirmedMeals?.reduce((acc, meal) => acc + meal.calories, 0) || 0
+
+    const proteinConsumed =
+      confirmedMeals?.reduce((acc, meal) => acc + meal.protein, 0) || 0
+    return (
+      <TableRow
+        key={`${entry.date} + ${index}`}
+        // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {entry.date}
+        </TableCell>
+        <TableCell align="right">{caloriesConsumed}</TableCell>
+        <TableCell align="right">{proteinConsumed}</TableCell>
+        <TableCell align="right">{entry.activityLevel}</TableCell>
+        <TableCell align="right">{entry.weight}</TableCell>
+      </TableRow>
+    )
+  })
+
   return (
     <>
-      <h1>LANDING PAGE</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell align="right">Activity&nbsp;Level</TableCell>
+              <TableCell align="right">Weight&nbsp;(lbs)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{generatedRows}</TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
